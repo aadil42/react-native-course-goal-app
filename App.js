3339f1982c7df04cf4bd2bf709a3cd24e4295a8a
aadil42 <adilkelawala@gmail.com>
2023-09-15 11:43:08 +0530

    added list text and styled elements

--- App.js
+++ App.js
@@ -8,23 +8,36 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Enter your goal." />
         <Button title="Add" />
       </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    padding: 40,
+    flex: 1
   },
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+    marginBottom: 24
   },
   textInput: {
     borderWidth: 1,
     paddingLeft: 5, 
     borderColor: '#cccccc',
     width: '80%',
-    marginRight: 8
+    marginRight: 8,
+  },
+
+  goalsContainer: {
+    flex: 5,
   }
 });
