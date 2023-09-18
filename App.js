8d3b60e0f06638a4868b344c6383e38463b1a6d6
aadil42 <adilkelawala@gmail.com>
2023-09-18 11:58:51 +0530

    testing changes

--- App.js
+++ App.js
@@ -21,6 +21,7 @@ export default function App() {
     setGoalList((currentGoalList) => {
       return currentGoalList.filter((goalItem) => goalItem.id !== id);
     });
+    // testing delete
   }
 
   return (
